8860538d57131bf6f0b6cb3dfa75552e1461ab1e
Jaiden Wu <41570385+JaidenWU@users.noreply.github.com>
2024-02-13 09:06:47 +0000

    Upload completed meal.py

--- 1-Conditionals/meal/meal.py
+++ 1-Conditionals/meal/meal.py
@@ -1,20 +1,20 @@
 def main():
     meal = input("What's the time? ")
-    hours, minutes = meal.split(":")
-    convert(meal)
-
-    """
+    meal = convert(meal)
     if 7 <= meal <= 8:
         print("breakfast time")
     elif 12 <= meal <= 13:
         print("lunch time")
     elif 18 <= meal <= 19:
         print("dinner time")
-    """
+    else:
+        print("nothing")
 
 
 def convert(time):
-    time = int(minutes) / 30 + int(hours)
+    hours, minutes = time.split(":")
+    time = float(hours) + float(minutes) / 60
+    return time
 
 
 main()
