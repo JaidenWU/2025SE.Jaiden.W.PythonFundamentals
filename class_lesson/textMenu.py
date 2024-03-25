343382970ca33de25fb3248f1f55f13bcc62340f
Jaiden Wu <41570385+JaidenWU@users.noreply.github.com>
2024-03-25 00:46:28 +0000

    Upload completed textMenu.py

---
+++ class_lesson/textMenu.py
@@ -0,0 +1,12 @@
+while True:
+    print("1) Selection 1 \n2) Selection 2 \n3) Selection 3\n4) Exit")
+    user = input("What would u like?")
+    if user == "1":
+        print("Anything Else?")
+    elif user == "2":
+        print("Anything Else?")
+    elif user == "3":
+        print("Anything Else?")
+    elif user == "4":
+        print("Thanks for using")
+        break
